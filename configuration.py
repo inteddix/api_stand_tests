5dfdec098fe2cd4934310fc0c4f72507b449808f
David Perez <dmtp1996@gmail.com>
2025-05-27 21:55:19 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-f42f7766-c3c9-4362-b399-2f9b8aa67ed2.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,12 @@
+import configuration
+import requests
+import data
+
+def post_products_kits(product_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,  # inserta la dirección URL completa
+                         json=product_ids,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+response = post_products_kits(data.product_ids);
+print(response.status_code)
+print(response.json())
